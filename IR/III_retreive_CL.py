88d85797d90d99b09d4eec9624d1da7c5c351341
crispinlang <crispin.lang@students.fhnw.ch>
2026-01-19 21:21:40 +0100

    Final version of the retrieval script, wrapping the searcher into a function with callable options for the logic.

--- IR/III_retreive_CL.py
+++ IR/III_retreive_CL.py
@@ -1,7 +1,6 @@
 from whoosh import index
-from II_index import index_dir
-from whoosh.qparser import QueryParser, MultifieldParser, OrGroup, FuzzyTermPlugin, PhrasePlugin
-
+from II_index_CL import index_dir
+from whoosh.qparser import QueryParser, MultifieldParser, OrGroup, FuzzyTermPlugin
 
 ix = index.open_dir(index_dir)
 
@@ -53,87 +52,3 @@ def run_search_experiment(ix, query_str, mode="MULTIFIELD"):
 #run_search_experiment(ix, "acid fluid", mode="OR")
 #run_search_experiment(ix, "acid fluid", mode="MULTIFIELD")
 run_search_experiment(ix, "fluiids~1", mode="FUZZY")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ##################################################
-# # Setting up the default AND logic retrieval
-# with ix.searcher() as s:
-#     qp = QueryParser("body", schema=ix.schema)
-#     q_AND = qp.parse(search_term)
-
-#     results = s.search(q_AND, limit=1)
-
-#     print("\n" + "="*30 + "\n")
-
-#     print(f"Found {len(results)} results for AND query logic: '{q_AND}'")
-
-#     for hit in results:
-#         print(f"Matched: {hit['title']}")
-#         print(f"Score:   {hit.score}")
-#         print("---")
-#     print("\n" + "="*30 + "\n")
-
-# ##################################################
-# # Setting up the OR logic retrieval
-# with ix.searcher() as s:
-#     qp = QueryParser("body", schema=ix.schema, group=OrGroup)
-#     q_OR = qp.parse(search_term) 
-
-#     results = s.search(q_OR, limit=1)
-
-#     print(f"Found {len(results)} results for OR query logic: '{q_OR}'")
-
-#     for hit in results:
-#         print(f"Matched: {hit['title']}")
-#         print(f"Score:   {hit.score}")
-#         print("---")
-#     print("\n" + "="*30 + "\n")
-
-# ##################################################
-# # Setting up the multifield logic retrieval
-# # mf = multifield
-
-# with ix.searcher() as s:
-#     mf_qp = MultifieldParser(["title", "body"], schema=ix.schema)
-#     q_MF = mf_qp.parse(search_term)
-
-#     results_mf = s.search(q_MF, limit=1)
-#     print(f"Found {len(results_mf)} results for MF query LOGIC: '{q_MF}'")
-
-#     for hit in results_mf:
-#         print(f"Matched: {hit['title']}")
-#         print(f"Score:   {hit.score}")
-#         print("---")
-
-#     print("\n" + "="*30 + "\n")
-
-# ##################################################
-# # Setting up the FUZZY retreival add-on
-# # mf_f_qp = multifield fuzzy searcher
-
-# with ix.searcher() as s:
-#     mf_qp.add_plugin(FuzzyTermPlugin()) 
-#     q_MF_fuzzy = mf_qp.parse(misspelled_search_term)
-
-#     results_mf_fuzzy = s.search(q_MF_fuzzy, limit=1)
-#     print(f"Found {len(results_mf_fuzzy)} results for query: '{q_MF_fuzzy}'")
-
-#     for hit in results_mf_fuzzy:
-#         print(f"Matched: {hit.get('title', 'Untitled')}")
-#         print(f"Score:   {hit.score}")
-#         print("---")
